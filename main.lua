eb7fad4dda06dcff104ab1f09ba13df0c3696df4
lukassemmler <kontakt@lukassemmler>
2022-03-16 23:21:18 +0100

    Add third debug config

--- main.lua
+++ main.lua
@@ -1,7 +1,12 @@
 -- Based on https://sheepolution.com/learn/book/bonus/vscode by Sheepolution, 2022
 
-if arg[2] == "debug" then
-    require("lldebugger").start()
+local launch_type = arg[2]
+if launch_type == "test" or launch_type == "debug" then
+    require "lldebugger"
+
+    if launch_type == "debug" then
+        lldebugger.start()
+    end
 end
 
 function love.draw()
@@ -13,6 +18,7 @@ local love_errorhandler = love.errhand
 
 function love.errorhandler(msg)
     if lldebugger then
+        lldebugger.start() -- Add this
         error(msg, 2)
     else
         return love_errorhandler(msg)
